6b19a5fe4631d004a2b357d6944cc0321f10fec6
iigmir <roc120j@gmail.com>
2023-01-10 02:15:57 +0800

    write io

---
+++ io.js
@@ -0,0 +1,12 @@
+import request from "./app/enc.js";
+import fs from "fs";
+
+const main = (entry) => {
+    request(entry).then( (d) => {
+        // console.log(d);
+        fs.writeFile(`./dist/${entry}.json`, JSON.stringify(d), up => { if(up) throw up; });
+    });
+};
+
+// eslint-disable-next-line no-undef
+main(process.argv[2]);
