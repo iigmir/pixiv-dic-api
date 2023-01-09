9a3182bb292755537c9d3c3a88e4ab2d8e8cbcb6
iigmir <roc120j@gmail.com>
2023-01-09 22:54:01 +0800

    WTFM

--- app/utils.js
+++ app/utils.js
@@ -1,14 +1,26 @@
+/**
+ * @typedef {Object} PixpediaBreadcumbInterface
+ * @property {String|null} name The item's name
+ * @property {String|null} position THe item's position
+ */
+
+/**
+ * Get a breadcrumb interface
+ * @param {Element} item
+ * @returns {PixpediaBreadcumbInterface}
+ */
+const generate_interface = item => ({
+    name: item.querySelector('*[itemprop="name"]').textContent,
+    position: item.querySelector('*[itemprop="position"]').content
+});
+
+/**
+ *
+ * @param {document} document
+ * @returns {PixpediaBreadcumbInterface[]}
+ */
 export const GenerateBreadcrumb = (document = document) => {
     const list = document.querySelectorAll('*[itemtype="http://schema.org/BreadcrumbList"] *[itemprop="itemListElement"]');
-    /**
-     * Get a breadcrumb interface
-     * @param {Element} item
-     * @returns {PixpediaBreadcumbInterface}
-     */
-    const generate_interface = item => ({
-        name: item.querySelector('*[itemprop="name"]').textContent,
-        position: item.querySelector('*[itemprop="position"]').content
-    });
     // [...list].length < 1 ? empty :
     return [...list].map( generate_interface );
 };
