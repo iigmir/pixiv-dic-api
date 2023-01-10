4a2f9dbec2cabf224eb63965f72cc6ddc65a6fcb
iigmir <roc120j@gmail.com>
2023-01-10 12:08:40 +0800

    Add GetImageMetadata

--- app/api/pixiv.js
+++ app/api/pixiv.js
@@ -1,4 +1,5 @@
 import request from "./ajax.js";
+import { JSDOM } from "jsdom";
 
 export const GetTag = (tag = "pixiv") => {
     const params = "lang=ja";
@@ -15,4 +16,22 @@ export const GetImageLinks = (id = "") => {
     const params = "lang=ja";
     const url = `https://www.pixiv.net/ajax/illust/${id}/pages?${params}`;
     return request(url);
-}
+};
+
+const GetImageEntry = (id = "") => {
+    const url = `https://www.pixiv.net/artworks/${id}`;
+    return request(url);
+};
+
+export const GetImageMetadata = (id = "") => {
+    return new Promise( (resolve, reject) => {
+        const ajax = GetImageEntry(id);
+        const render = (html = "<html></html>") => {
+            const { document } = (new JSDOM(html)).window;
+            const text = document.querySelector("#meta-preload-data")?.content ?? "{}";
+            resolve(JSON.parse(text));
+        };
+        const failed = (error) => reject(error);
+        ajax.then( render ).catch( failed );
+    });
+};
