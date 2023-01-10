99f196624c726bd588bff6fb0e08e25fd6ec051d
iigmir <roc120j@gmail.com>
2023-01-10 15:49:53 +0800

    Set PixivImageDatas

--- app/ImageInfo/images.js
+++ app/ImageInfo/images.js
@@ -1,4 +1,19 @@
+/**
+ * @typedef {Object} PixivImageDataInterface
+ * @property {Object} urls Image URL paths
+ * @property {Number} width Image width
+ * @property {Number} height Image height
+ */
+
+/**
+ * If you want to active image datas, set `ask_image_link` to `true`.
+ *
+ * The interface returns `null` if `ask_image_link` is, by default, `false`.
+ */
 class PixivImageDatas {
+    /**
+     * @returns {PixivImageDataInterface}
+     */
     data = {
         "urls": {
             "thumb_mini": "",
@@ -13,8 +28,12 @@ class PixivImageDatas {
     get image_link() {
         return this.data;
     }
+    /**
+     * The interface.
+     * @returns {PixivImageDataInterface|null}
+     */
     get result() {
-        return this.data;
+        return this.ask_image_link ? this.data : null;
     }
 }
 
