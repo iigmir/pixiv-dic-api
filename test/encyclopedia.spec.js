eddfbd0fad84d7a939e7bc1a114ed70f97994286
iigmir <roc120j@gmail.com>
2023-01-10 10:55:46 +0800

    Parser

--- test/encyclopedia.spec.js
+++ test/encyclopedia.spec.js
@@ -2,35 +2,33 @@ import main from "../app/index.js";
 import { strictEqual, deepStrictEqual } from "assert";
 
 describe("Parser module", () => {
-    describe("when the entry is vaild", () => {
-        const vaild_input = "園田海未";
-        it("should return a summary", async () => {
-            // Infos
-            const expected = "『ラブライブ!』の登場人物。通称「海未ちゃん」。";
-            // Functions
-            const response = await main(vaild_input);
-            const abstract = response.summary.abstract;
-            strictEqual( abstract, expected );
-        });
-        it("should return a breadcrumb", async () => {
-            // Infos
-            const expected = {
-                name: "ピクシブ百科事典",
-                position: 1
-            };
-            // Functions
-            const response = await main(vaild_input);
-            const breadcrumb = response.breadcrumb;
-            deepStrictEqual( breadcrumb[0], expected );
-        });
-        it("should return content", async () => {
-            // Infos
-            const expected = 8;
-            // Functions
-            const response = await main(vaild_input);
-            const content = response.content;
-            strictEqual( content.length, expected );
-        });
+    const vaild_input = "園田海未";
+    it("should return a summary", async () => {
+        // Infos
+        const expected = "『ラブライブ!』の登場人物。通称「海未ちゃん」。";
+        // Functions
+        const response = await main(vaild_input);
+        const abstract = response.summary.abstract;
+        strictEqual( abstract, expected );
+    });
+    it("should return a breadcrumb", async () => {
+        // Infos
+        const expected = {
+            name: "ピクシブ百科事典",
+            position: 1
+        };
+        // Functions
+        const response = await main(vaild_input);
+        const breadcrumb = response.breadcrumb;
+        deepStrictEqual( breadcrumb[0], expected );
+    });
+    it("should return content", async () => {
+        // Infos
+        const expected = 8;
+        // Functions
+        const response = await main(vaild_input);
+        const content = response.content;
+        strictEqual( content.length, expected );
     });
 });
 
