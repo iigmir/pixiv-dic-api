303ef742575605006f4d4a372701cf8185bfd54f
iigmir <roc120j@gmail.com>
2023-01-11 01:37:49 +0800

    Text

--- test/content.spec.js
+++ test/content.spec.js
@@ -5,48 +5,48 @@ const is_stable = response => response.status.code === 200;
 
 describe("Content module", () => {
     const vaild_input = "園田海未";
-    it("should have a HTML source", async () => {
-        // Infos
-        const expected = "string";
-        // Functions
-        const response = await main(vaild_input);
-        if( is_stable(response) ) {
-            const content = response.content[0].contents[0];
-            strictEqual( typeof(content.source), expected );
-        } else {
-            strictEqual( response.status.message, "normal" );
-        }
-    });
-    it("should have an image if it is an image", async () => {
-        // Infos
-        const expected = "49284361";
-        // Functions
-        const response = await main(vaild_input);
-        if( is_stable(response) ) {
-            const content = response.content[2].contents[0];
-            strictEqual( content.image.id, expected );
-        } else {
-            strictEqual( response.status.message, "normal" );
-        }
-    });
-    // it("should return a breadcrumb", async () => {
-    //     // Infos
-    //     const expected = {
-    //         name: "ピクシブ百科事典",
-    //         position: 1
-    //     };
-    //     // Functions
-    //     const response = await main(vaild_input);
-    //     const breadcrumb = response.breadcrumb;
-    //     deepStrictEqual( breadcrumb[0], expected );
-    // });
-    // it("should return content", async () => {
-    //     // Infos
-    //     const expected = 8;
-    //     // Functions
-    //     const response = await main(vaild_input);
-    //     const content = response.content;
-    //     strictEqual( content.length, expected );
-    // });
-});
+    describe( "Basic", () => {
+        it("should have a HTML source", async () => {
+            // Infos
+            const expected = "string";
+            // Functions
+            const response = await main(vaild_input);
+            if( is_stable(response) ) {
+                const content = response.content[0].contents[0];
+                strictEqual( typeof(content.source), expected );
+            } else {
+                strictEqual( response.status.message, "normal" );
+            }
+        });
+    } );
+    describe( "Image interface", () => {
+        it("should have an image if it is an image", async () => {
+            // Infos
+            const expected = "49284361";
+            // Functions
+            const response = await main(vaild_input);
+            if( is_stable(response) ) {
+                const content = response.content[2].contents[0];
+                strictEqual( content.image.id, expected );
+            } else {
+                strictEqual( response.status.message, "normal" );
+            }
+        });
+    } );
+    describe( "Text", () => {
+        it("should have a text if it is a text", async () => {
+            // Infos
+            const expected = "「それが穂乃果の凄い所なんです。わたしもことりも、μ'sのみんなもそう思っています」";
+            // Functions
+            const response = await main(vaild_input);
+            if( is_stable(response) ) {
+                const content = response.content[0].contents[3];
+                strictEqual( content.text, expected );
+            } else {
+                strictEqual( response.status.message, "normal" );
+            }
+        });
+    } );
+    describe( "Basic", () => {} );
 
+});
