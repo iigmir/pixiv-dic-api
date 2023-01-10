d07c054be8209439886d0f42765a9a703c148eb3
iigmir <roc120j@gmail.com>
2023-01-10 13:50:36 +0800

    PixivAuthorInfo

--- app/Encyclopedia/image.js
+++ app/Encyclopedia/image.js
@@ -1,24 +1,44 @@
 import { JSDOM } from "jsdom";
 
-
-/**
- * @param {String} input URL
- * @returns ID
- */
-const get_author_id = (input = "https://example.com?id=0") => {
-    const param = new URLSearchParams(input);
-    const qsid = param.get("id");
-    /**
-     * Format is https://pixiv.net/users/0
-     */
-    if( qsid == null ) {
-        const path = (new URL(input)).pathname;
-        const paths = path.split("/");
-        const is_user = paths[1] === "users";
-        return is_user ? paths[2] : "0";
+class PixivAuthorInfo {
+    data = {
+        "id": "",
+        "name": ""
     }
-    return qsid ?? "0";
-};
+    get author() {
+        return this.data;
+    }
+    set author({ id = "", name = "" }) {
+        this.data = { id, name };
+    }
+    /**
+     * @param {String} input URL
+     * @returns ID
+     */
+    get_author_id(input = "https://example.com?id=0") {
+        const param = new URLSearchParams(input);
+        const qsid = param.get("id");
+        /**
+         * Format is https://pixiv.net/users/0
+         */
+        if( qsid == null ) {
+            const path = (new URL(input)).pathname;
+            const paths = path.split("/");
+            const is_user = paths[1] === "users";
+            return is_user ? paths[2] : "0";
+        }
+        return qsid ?? "0";
+    }
+    set_author_by_dom(dataset = { authorUrl: "http://www.pixiv.net/member.php?id=0", authorName: "" }) {
+        this.data = {
+            id: this.get_author_id(dataset.authorUrl),
+            name: dataset.authorName,
+        };
+    }
+    get result() {
+        return this.author;
+    }
+}
 
 class PixivImageInfo {
     constructor() {
@@ -59,22 +79,15 @@ class PixivImageInfo {
         this.set_id_by_dom();
     }
     // Author datas
-    author_info = {
-        "id": "",
-        "name": ""
+    author_info = new PixivAuthorInfo()
+    get author() {
+        return this.author_info.result;
     }
     set author({ id = "", name = "" }) {
-        this.author_info = { id, name };
+        this.author_info.author = { id, name };
     }
     set_author_by_dom() {
-        const dataset = this.document_link.dataset;
-        this.author = {
-            id: get_author_id(dataset.authorUrl),
-            name: dataset.authorName,
-        };
-    }
-    get author() {
-        return this.author_info;
+        this.author_info.set_author_by_dom( this.document_link.dataset )
     }
     // Image link info
     image_links = {
