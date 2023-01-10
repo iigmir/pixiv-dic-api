8021e5c4a91de6d816a5026cb20e7f172479746d
iigmir <roc120j@gmail.com>
2023-01-10 11:31:53 +0800

    GetImageLinks API

--- app/api/pixiv.js
+++ app/api/pixiv.js
@@ -10,3 +10,9 @@ export const GetEncyclopediaEntry = (tag = "pixiv") => {
     const url = `https://dic.pixiv.net/a/${tag}`;
     return request(url);
 };
+
+export const GetImageLinks = (id = "") => {
+    const params = "lang=ja";
+    const url = `https://www.pixiv.net/ajax/illust/${id}/pages?${params}`;
+    return request(url);
+}
