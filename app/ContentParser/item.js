20689c6ca6fd2239d1c84d3b60b59ceb1805d8cb
iigmir <roc120j@gmail.com>
2023-01-10 23:04:36 +0800

    Move item interface out

---
+++ app/ContentParser/item.js
@@ -0,0 +1,72 @@
+import { GetInageByEmbedimage } from "../Encyclopedia/imagescripts.js";
+
+/**
+ * @typedef {Object} PixpediaSectionContentInterface
+ * @property {String} source The HTML
+ * @property {PixivImageInfoInterface|null} [image] If the content is an embedded image, return an object. Else, returns null.
+ */
+
+class SectionItemInterface {
+    constructor(title = "Preface", contents = []) {
+        this.title = title;
+        this.contents = contents;
+    }
+    add_content(input = {}) {
+        this.contents = [...this.contents, input];
+    }
+    set_title(input = "") {
+        this.title = input;
+    }
+    reset_data() {
+        this.title = "";
+        this.contents = [];
+    }
+    get result() {
+        return {
+            title: this.title,
+            contents: this.contents,
+        };
+    }
+}
+
+class PixpediaContentItems {
+    dom = null
+    constructor(dom = [Element]) {
+        this.dom = dom;
+    }
+    /**
+     * COntent interface
+     * @param {Element} dom
+     * @returns {PixpediaSectionContentInterface}
+     */
+    generate_content_interface(dom = Element) {
+        const source = dom.outerHTML.trim();
+        const image = GetInageByEmbedimage( source );
+        return { source, image };
+    }
+    get ary() {
+        let result = [];
+        let section = new SectionItemInterface("Preface", []);
+        const action = (dom = Element, index = 0, articles = []) => {
+            // States
+            const is_new_title = dom.nodeName === "H2";
+            const is_last_section = index + 1 === articles.length;
+            const new_content = this.generate_content_interface(dom);
+            // Actions
+            if (is_new_title) {
+                result.push( section.result );
+                section.reset_data();
+                section.title = dom.textContent.trim();
+            } else {
+                section.add_content( new_content );
+            }
+            if (is_last_section) {
+                result.push(section.result);
+            }
+        };
+        this.dom.forEach( action );
+        return result;
+    }
+}
+
+export default PixpediaContentItems;
