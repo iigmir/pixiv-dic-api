8eb148600d7f097c34ce79117c78212f4523bfd1
iigmir <roc120j@gmail.com>
2023-01-11 15:35:55 +0800

    Text

--- app/TextParser/script.js
+++ app/TextParser/script.js
@@ -1,12 +1,20 @@
 import { JSDOM } from "jsdom";
 
+const should_not_parse = (htmltext = "<html></html>") => {
+    const regex = /(^<h1)|(^<h2)(^<h3)|(^<h4)|(^<h5)|(^<h6)|(^<table)|(^<ul)|(d_toparticle_flux_ad)/g;
+    return regex.test( htmltext );
+};
+
 /**
- * `<p>Foobar</P>` => `Foobar`.
+ * `<p>Foobar</p>` => `Foobar`.
  * @param {String} htmltext HTML text
  */
 export const GetTextByHtml = (htmltext = "<html></html>") => {
+    if( should_not_parse(htmltext) ) {
+        return null;
+    }
     const dom = new JSDOM(htmltext);
     const source = dom.window.document.body.textContent.trim();
     const rendered = source.split("\n\n").filter( i => i.trim() !== "" );
-    return rendered;
+    return rendered.length > 0 ? rendered : [];
 };
