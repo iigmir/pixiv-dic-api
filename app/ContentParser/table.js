6095fbb64aea5c270da62f1a24b78f914f50c308
iigmir <roc120j@gmail.com>
2023-01-12 16:32:55 +0800

    ASSVA

--- app/ContentParser/table.js
+++ app/ContentParser/table.js
@@ -3,7 +3,10 @@ import { Tabletojson } from "tabletojson";
 export const GetTableData = (input = "<div></div>") => {
     try {
         if( /(^<table)/g.test( input ) ) {
-            return Tabletojson.convert( input );
+            const options = {
+                useFirstRowForHeadings: false,
+            }
+            return Tabletojson.convert( input, options );
         }
         return null;
     } catch (error) {
