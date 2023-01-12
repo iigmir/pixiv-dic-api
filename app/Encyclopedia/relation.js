c91083737040aac1fc4e2a32a5a21ba3b2e32f1b
iigmir <roc120j@gmail.com>
2023-01-12 21:28:59 +0800

    Develop relation

--- app/Encyclopedia/relation.js
+++ app/Encyclopedia/relation.js
@@ -1,10 +1,25 @@
 class EntryRelationship {
+    constructor(document = Document) {
+        this.document = document;
+    }
+    get relation_main() {
+        return this.document.querySelector("#article-relation");
+    }
+    get items() {
+        return this.relation_main.querySelectorAll(".relation > .items");
+    }
+    get_links(element = Element) {
+        const elements = element.querySelectorAll("a");
+        const get_text = i => i.textContent.trim();
+        const has_text = t => t !== "" && t !== "もっと見る";
+        return [...elements].map(get_text).filter(has_text);
+    }
     get result() {
         return {
-            parents: [],
-            children: [],
-            siblings: [],
-            categories: [],
+            parents: this.get_links(this.items[0]),
+            children: this.get_links(this.items[1]),
+            siblings: this.get_links(this.items[2]),
+            categories: this.get_links(this.relation_main.children[3])
         };
     }
 }
