66292a79e813c45e997be921681eed39aa8f0919
iigmir <roc120j@gmail.com>
2023-01-22 08:25:14 +0800

    I promise to you, too

--- io.js
+++ io.js
@@ -2,9 +2,16 @@ import request from "./app/index.js";
 import fs from "fs";
 
 const main = (entry) => {
-    request(entry).then( (d) => {
-        // console.log(d);
-        fs.writeFile(`./dist/${entry}.json`, JSON.stringify(d), up => { if(up) throw up; });
+    return new Promise( (resolve, reject) => {
+        request(entry).then( (d) => {
+            const action = (data) => error => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(data);
+            };
+            fs.writeFile(`./dist/${entry}.json`, JSON.stringify(d), action(d) );
+        }).catch( error => reject(error) );
     });
 };
 
